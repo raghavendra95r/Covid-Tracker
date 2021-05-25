8581e57e2c34932c4bd148344fb1e0240610c36a
raghavendra95r <rrgowda95@gmail.com>
2021-05-25 13:27:20 +0530

    line chart added

--- covid19/src/components/Charts/Charts.js
+++ covid19/src/components/Charts/Charts.js
@@ -26,6 +26,7 @@ const  Charts =  ({data : {confirmed,deaths, recovered}, country}) =>  {
                   title: { display: true, text: `Current state in ${country}` },
                   backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', '#e60d0d'],
                   data: [confirmed.value, recovered.value, deaths.value],
+                  font: '5px'
                 },
               ],
             }}
@@ -37,39 +38,33 @@ const  Charts =  ({data : {confirmed,deaths, recovered}, country}) =>  {
         ) : null
       );
     
-    //   const lineChart = (
-    //     dailyData[0] ? (
-    //       <Line
-    //         data={{
-    //           labels: dailyData.map(({ date }) => new Date(date).toLocaleDateString()),
-    //           datasets: [{
-    //             data: dailyData.map((data) => data.confirmed),
-    //             label: 'Infected',
-    //             borderColor: '#3333ff',
-    //             fill: true,
-    //           }, {
-    //             data: dailyData.map((data) => data.deaths),
-    //             label: 'Deaths',
-    //             borderColor: 'red',
-    //             backgroundColor: 'rgba(255, 0, 0, 0.5)',
-    //             fill: true,
-    //           },  {
-    //             data: dailyData.map((data) => data.recovered),
-    //             label: 'Recovered',
-    //             borderColor: 'green',
-    //             backgroundColor: 'rgba(0, 255, 0, 0.5)',
-    //             fill: true,
-    //           },
-    //           ],
-    //         }}
-    //       />
-    //     ) : null
-    //   );
+      const lineChart = (
+        dailyData[0] ? (
+          <Line
+            data={{
+              labels: dailyData.map(({ date }) => new Date(date).toLocaleDateString()),
+              datasets: [{
+                data: dailyData.map((data) => data.confirmed),
+                label: 'Infected',
+                borderColor: '#3333ff',
+                fill: true,
+              }, {
+                data: dailyData.map((data) => data.deaths),
+                label: 'Deaths',
+                borderColor: 'red',
+                backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                fill: true,
+              }
+              ],
+            }}
+          />
+        ) : null
+      );
     
     return (
         <div className="container">
-           {/* {country ? barChart : lineChart}  */}
-           {barChart }
+           {country ? barChart : lineChart} 
+           {/* {barChart } */}
         
         </div>
     )
